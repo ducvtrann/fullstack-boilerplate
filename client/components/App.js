d9fe049f517021e9f8b637ccca9509a9acec78d1
Duc Tran <ducvtrann@gmail.com>
2020-09-07 11:49:12 -0400

    Update server/index to handle errors and add morgan for logging

--- client/components/App.js
+++ client/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return <div>Hello World</div>;
 };
 
 export default hot(module)(App);
